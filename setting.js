fb19f8835c3add93bb5ac08fffca4b5b24eeec01
Shafiullah90 <shafiullahpathan704@gmail.com>
2025-08-07 12:41:59 +0430

    Create setting.js

---
+++ setting.js
@@ -0,0 +1,13 @@
+const settings = {
+  packname: 'shafi king',
+  author: 'Shafi king',
+  botName: "KING-BOT",
+  botOwner: 'Shafi pathan', // Your name
+  ownerNumber: '66620925025', //Set your number here without + symbol.
+  giphyApiKey: 'qnl7ssQChTdPjsKta2Ax2LMaGXz303tq',
+  commandMode: "public",
+  description: "This is a bot for managing group commands and automating tasks.",
+  version: "1.0.0",
+};
+
+module.exports = settings;
