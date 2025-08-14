12e3bdaf6151e8dc8dc7e8eb7509064f4de94833
Shafiullah90 <shafiullahpathan704@gmail.com>
2025-08-14 15:45:31 +0430

    Update settings.js

--- settings.js
+++ settings.js
@@ -1,14 +1,5 @@
-const settings = {
-  packname: '❤️Pathan boy❤️',
-  author: 'Shafi king',
-  botName: "Pathan-MD",
-  botOwner: 'Shafi pathan', // Your name
-  ownerNumber: '93703802176', //Set your number here without + symbol.
-  giphyApiKey: 'qnl7ssQChTdPjsKta2Ax2LMaGXz303tq',
-  commandMode: "public",
-  description: "This is a bot for managing group commands and automating tasks.",
-  version: "1.0.0",
+module.exports = {
+  session: '',
+  botName: 'My WhatsApp Bot',
+  ownerNumber: '93703802176',//YOUR_PHONE_NUMBER_WITH_COUNTRY_CODE
 };
-
-module.exports = settings;
-
