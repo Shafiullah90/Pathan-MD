cdf6fd706e511a5121b55d1447606a13b99b9add
Shafiullah90 <shafiullahpathan704@gmail.com>
2025-08-14 15:47:54 +0430

    Update main.js

--- main.js
+++ main.js
@@ -1,1 +1,23 @@
+const { Client } = require('@baileys/whatsapp-web.js');
+const settings = require('./settings');
+const client = new Client();
+client.initialize();
+const { Client } = require('@baileys/whatsapp-web.js');
+const settings = require('./settings');
+const client = new Client();
+client.on('qr', (qr) => {
+  console.log('QR RECEIVED', qr);
+});
+client.on('ready', () => {
+  console.log('BOT IS READY!');
+});
+client.on('message', (message) => {
+  console.log('MESSAGE RECEIVED', message);
+  // Command handler will go here
+});
+client.initialize();
 
+// commands handler 
+
+
+const commandHandler = require('./lib/commandHandler');
